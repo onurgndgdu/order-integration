098f881a5f8f5d80a95e51266e82edb4754afe40
gundogduonur <gundogduonur@outlook.com.tr>
2023-08-22 01:32:35 +0300

    added getter and setter in model

--- src/main/java/com/onurgundogdu/orderintegration/OrderIntegrationApplication.java
+++ src/main/java/com/onurgundogdu/orderintegration/OrderIntegrationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrderIntegrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderIntegrationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderIntegrationApplication.class, args);
+    }
 
 }
--- src/main/java/com/onurgundogdu/orderintegration/model/Order.java
+++ src/main/java/com/onurgundogdu/orderintegration/model/Order.java
@@ -1,2 +1,31 @@
-package com.onurgundogdu.orderintegration.model;public class Order {
+package com.onurgundogdu.orderintegration.model;
+
+public class Order {
+    private String orderId;
+    private String custId;
+    private double totalPrice;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
